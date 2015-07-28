ce06c629cea1ab5b56b7182ace3eabee875857eb
Cody Deckard <cjdeckard@gmail.com>
2015-07-28 19:18:52 +0000

    unit migration

--- db/migrate/20150728190752_create_units.rb
+++ db/migrate/20150728190752_create_units.rb
@@ -1,7 +1,9 @@
 class CreateUnits < ActiveRecord::Migration
   def change
     create_table :units do |t|
-
+      t.string :address
+      t.integer :dues
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
