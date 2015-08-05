566d740b765b8e5c8558a2640004575bbf44fce0
Cody Deckard <cjdeckard@gmail.com>
2015-08-05 16:03:02 +0000

    Added user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  #validates :email, presence: true, uniqueness: true
+  
   has_one :unit
   
   def admin?
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,21 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Should not save user without email address" do
+    me = User.new
+    assert_not me.save, "User saved without an email"
+  end
+  
+  test "Emails should be unique" do
+    
+      tom = User.new(:email =>"tom@test-domain.com", :encrypted_password => "password")
+      tom.save
+      fred = User.new(:email => "tom@test-domain.com", :encrypted_password => "password2")
+      assert_not fred.save, "User saved with the same email address"
+      
+    
+    
+  end
+  
+  
 end
