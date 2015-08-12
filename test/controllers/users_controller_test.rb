4c942b010c9f7afa5600af1cd64b6f2ff872b7e3
Cody Deckard <cjdeckard@gmail.com>
2015-08-12 14:58:23 +0000

    User who is logged in, not admin redirect test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,4 +12,21 @@ class UsersControllerTest < ActionController::TestCase
     assert_response(:redirect)
     
   end
+  
+  test "Users who are not admin cannot access the user overview" do
+    
+    tom = users(:tom)
+    
+    sign_in tom
+    
+    get :index
+    
+    assert_response(:redirect)
+    
+  end
+  
+  test "Users who are admin can access the user overview" do
+    
+  end
+  
 end
