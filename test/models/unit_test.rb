43ed0301a24369ee542491f7ec5a49ea759cff36
Cody Deckard <cjdeckard@gmail.com>
2015-11-23 19:54:15 +0000

    Added testing scenario for users only being able to be assigned to one unit at a time

--- test/models/unit_test.rb
+++ test/models/unit_test.rb
@@ -4,4 +4,12 @@ class UnitTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "A user can only be assigned to one unit at a time" do
+    three = units(:three)
+    three.user_id = users(:pres).id 
+    three.save
+    assert_equal units(:four).user_id, 0
+    
+  end
 end
